bacbe25bf6e7f4aef167feb4d61066367596292e
Eugene <peugval@gmail.com>
2019-10-18 11:15:36 +0100

    Fixing setup.py

--- kdl_parser_py/setup.py
+++ kdl_parser_py/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
    packages=['kdl_parser_py'],
-   package_dir={'': ''}
+   package_dir={'': '.'}
 )
 
 setup(**d)
